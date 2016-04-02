08ecccc3facbcb682c814f76d5055b72486af9c0
ryannnnnn <ranklyyarns@gmail.com>
2016-04-01 19:19:27 -0700

    Added files via upload

---
+++ InputValidator.java
@@ -0,0 +1,38 @@
+
+public class InputValidator {
+
+	static InputValidator sharedInstance = new InputValidator();
+	private InputValidator(){};
+	
+	boolean validateName(String name)
+	{
+		boolean valid = true;
+		
+		
+		return valid;
+	}
+	
+	boolean validateSSN(String ssn)
+	{
+		boolean valid = true;
+		
+		
+		return valid;
+	}
+	
+	boolean validateSalary(int salary)
+	{
+		boolean valid = true;
+		
+		
+		return valid;
+	}
+	
+	boolean validatePhoneNumber(String number)
+	{
+		boolean valid = true;
+		
+		
+		return valid;
+	}
+}
